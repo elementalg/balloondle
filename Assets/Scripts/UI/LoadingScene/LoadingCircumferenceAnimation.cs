683e033ca79efc1422c0607b6f8fec5633ad35ce
Gabriel Ami <gamihalachioaie@gmail.com>
2021-05-30 15:48:56 +0200

    add: upgrade Unity version

---
+++ Assets/Scripts/UI/LoadingScene/LoadingCircumferenceAnimation.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace Balloondle.UI.LoadingScene
+{
+    public class LoadingCircumferenceAnimation : MonoBehaviour
+    {
+        private Transform loadingIndicatorState;
+
+        [SerializeField]
+        private float zRotationPerFrame;
+
+        /// <summary>
+        /// Retrieves the transform component from the parent GameObject.
+        /// </summary>
+        void Start()
+        {
+            loadingIndicatorState = gameObject.transform;
+        }
+
+        /// <summary>
+        /// Rotates the loading indicator.
+        /// </summary>
+        void Update()
+        {
+            loadingIndicatorState.Rotate(0f, 0f, zRotationPerFrame);
+        }
+    }
+}
