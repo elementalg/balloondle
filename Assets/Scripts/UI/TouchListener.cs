3e007a379a67e869550c46bdf803893cec793b84
Gabriel Ami <gamihalachioaie@gmail.com>
2021-07-09 15:49:26 +0200

    add: listener which provides easy access to detecting touches' states

---
+++ Assets/Scripts/UI/TouchListener.cs
@@ -0,0 +1,69 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
+using TouchPhase = UnityEngine.InputSystem.TouchPhase;
+
+namespace Balloondle.UI
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class TouchListener : MonoBehaviour
+    {
+        public Action<Touch> OnTouchBegan;
+        public Action<Touch> OnTouchDrag;
+        public Action<Touch> OnTouchEnded;
+        
+        /// <summary>
+        /// Enable enhanced touch support at start.
+        /// </summary>
+        private void Start()
+        {
+            EnhancedTouchSupport.Enable();
+        }
+
+        /// <summary>
+        /// Check all active touches, and call actions based on each touch's phase.
+        /// </summary>
+        private void Update()
+        {
+            foreach (Touch touch in Touch.activeTouches)
+            {
+                switch (touch.phase)
+                {
+                    case TouchPhase.Began:
+                        if (OnTouchBegan != null)
+                        {
+                            OnTouchBegan(touch);
+                        }
+
+                        break;
+                    case TouchPhase.Moved:
+                        if (OnTouchDrag != null)
+                        {
+                            OnTouchDrag(touch);
+                        }
+
+                        break;
+                    case TouchPhase.Canceled:
+                    case TouchPhase.Ended:
+                        if (OnTouchEnded != null)
+                        {
+                            OnTouchEnded(touch);
+                        }
+
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Disable enhanced touch support when disabled.
+        /// </summary>
+        private void OnDisable()
+        {
+            EnhancedTouchSupport.Disable();
+        }
+    }
+}
