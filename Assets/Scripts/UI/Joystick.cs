83b6806446602c5d1312aa283fd4f4301377f659
Gabriel Ami <gamihalachioaie@gmail.com>
2021-07-10 20:13:41 +0200

    add: base logic for screen joystick

---
+++ Assets/Scripts/UI/Joystick.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace Balloondle.UI
+{
+    public class Joystick : MonoBehaviour
+    {
+        private RectTransform _parentRectTransform;
+        private Rect _rect;
+
+        public void Start()
+        {
+            if (transform.parent.GetComponent<RectTransform>() == null)
+            {
+                throw new InvalidOperationException("Joystick requires parent to have a RectTransform.");
+            }
+
+            _parentRectTransform = transform.parent.GetComponent<RectTransform>();
+
+            if (GetComponent<RectTransform>()?.rect == null)
+            {
+                throw new InvalidOperationException("Joystick requires GameObject to have a RectTransform.");
+            }
+            
+            _rect = GetComponent<RectTransform>().rect;
+        }
+        
+        public bool IsScreenPointWithinBounds(Vector2 screenPoint, Camera gameCamera)
+        {
+            return CustomRectUtils.Instance.IsScreenPointWithinRect(
+                screenPoint,
+                gameCamera,
+                _parentRectTransform,
+                _rect);
+        }
+    }
+}
