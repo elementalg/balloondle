3b7d5708246f4dc7f2891e4771e370564aa9273d
Gabriel Ami <gamihalachioaie@gmail.com>
2021-07-11 16:34:36 +0200

    add, test: ignore upcoming touches if already listening to an ongoing touch

--- Assets/Scripts/UI/JoystickMoverLogic.cs
+++ Assets/Scripts/UI/JoystickMoverLogic.cs
@@ -1,12 +1,21 @@
-﻿namespace Balloondle.UI
+﻿using JetBrains.Annotations;
+
+namespace Balloondle.UI
 {
     public class JoystickMoverLogic
     {
+        [CanBeNull] 
         public Touch ListenedTouch { get; private set; }
 
-        public void ListenToTouch(Touch touch)
+        public bool ListenToTouch(Touch touch)
         {
+            if (ListenedTouch != null)
+            {
+                return false;
+            }
+
             ListenedTouch = touch;
+            return true;
         }
     }
 }
--- Assets/Tests/UI/JoystickMoverLogicTest.cs
+++ Assets/Tests/UI/JoystickMoverLogicTest.cs
@@ -18,9 +18,22 @@ namespace Tests.UI
         {
             Touch touch = new Touch(1); // '1' has no special meaning.
 
-            _joystickMoverLogic.ListenToTouch(touch);
+            Assert.True(_joystickMoverLogic.ListenToTouch(touch));
             
             Assert.AreEqual(touch.touchId, _joystickMoverLogic.ListenedTouch.touchId);
+        }
+
+        [Test]
+        public void IgnoresTouchIfListeningAlreadyToAOnGoingTouch()
+        {
+            // The numbers used as ID have no special meaning.
+            Touch listenedTouch = new Touch(1);
+            Touch notListenedTouch = new Touch(2);
+            
+            Assert.True(_joystickMoverLogic.ListenToTouch(listenedTouch));
+            Assert.False(_joystickMoverLogic.ListenToTouch(notListenedTouch));
+            
+            Assert.AreEqual(listenedTouch.touchId, _joystickMoverLogic.ListenedTouch.touchId);
         }
     }
 }
