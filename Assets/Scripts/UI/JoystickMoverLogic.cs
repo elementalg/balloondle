a873041da78a0ef82b796f678650e23451a20c43
Gabriel Ami <gamihalachioaie@gmail.com>
2021-07-11 16:37:02 +0200

    doc: describe functionality of ListenToTouch

--- Assets/Scripts/UI/JoystickMoverLogic.cs
+++ Assets/Scripts/UI/JoystickMoverLogic.cs
@@ -7,6 +7,11 @@ namespace Balloondle.UI
         [CanBeNull] 
         public Touch ListenedTouch { get; private set; }
 
+        /// <summary>
+        /// Starts listening to a touch, only if not already listening to an ongoing one, otherwise it gets ignored.
+        /// </summary>
+        /// <param name="touch"></param>
+        /// <returns>True if started listening to the passed touch, false if it got ignored.</returns>
         public bool ListenToTouch(Touch touch)
         {
             if (ListenedTouch != null)
