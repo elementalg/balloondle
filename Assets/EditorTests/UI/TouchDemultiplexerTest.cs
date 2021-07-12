29be4a531e0ae349ac8278e7a7206f48ada7a212
Gabriel Ami <gamihalachioaie@gmail.com>
2021-07-12 20:52:21 +0200

    add: base for a touch demultiplexer, which reserves touches by their id for a single action

---
+++ Assets/EditorTests/UI/TouchDemultiplexerTest.cs
@@ -0,0 +1,85 @@
+﻿using Balloondle.UI;
+using NUnit.Framework;
+using UnityEngine.InputSystem;
+
+namespace EditorTests.UI
+{
+    public class TouchDemultiplexerTest
+    {
+        private TouchDemultiplexer _touchDemultiplexer;
+
+        [SetUp]
+        public void Initialize()
+        {
+            _touchDemultiplexer = new TouchDemultiplexer();
+        }
+        
+        [Test]
+        public void TransfersTouchToFirstOutput()
+        {
+            bool outputCalled = false;
+            Touch touchThatJustBegun = new Touch(1) {phase = TouchPhase.Began};
+
+            _touchDemultiplexer.AddOutputToQueue(touch =>
+            {
+                outputCalled = true;
+            });
+            _touchDemultiplexer.OnTouchUpdate(touchThatJustBegun);
+            
+            Assert.True(outputCalled, "Output has not been called.");
+        }
+
+        [Test]
+        public void TransfersTouchOnlyToSelectedOutput()
+        {
+            // Random numbers used as id.
+            Touch touchForFirstOutput = new Touch(1431) {phase = TouchPhase.Began};
+            Touch touchForSecondOutput = new Touch(5654) {phase = TouchPhase.Began};
+            bool firstOutputCorrect = false;
+            bool secondOutputCorrect = false;
+            
+            _touchDemultiplexer.AddOutputToQueue(touch =>
+            {
+                firstOutputCorrect = (touch.touchId == touchForFirstOutput.touchId);
+            });
+            _touchDemultiplexer.AddOutputToQueue(touch =>
+            {
+                secondOutputCorrect = (touch.touchId == touchForSecondOutput.touchId);
+            });
+            _touchDemultiplexer.OnTouchUpdate(touchForFirstOutput);
+            _touchDemultiplexer.OnTouchUpdate(touchForFirstOutput);
+            _touchDemultiplexer.OnTouchUpdate(touchForSecondOutput);
+            _touchDemultiplexer.OnTouchUpdate(touchForFirstOutput);
+            
+            Assert.True(firstOutputCorrect, "First output has received touch updates from " +
+                                            "other touches not assigned to it.");
+            Assert.True(secondOutputCorrect, "Second output has received touch updates from " +
+                                             "other touches not assigned to it.");
+        }
+
+        [Test]
+        public void DoesNotTransferTouchAfterTouchEnded()
+        {
+            const int expectedCallCount = 3;
+            int callCount = 0;
+            Touch touchWhichWillEnd = new Touch(32) {phase = TouchPhase.Began};
+            
+            _touchDemultiplexer.AddOutputToQueue(touch =>
+            {
+                callCount++;
+            });
+            _touchDemultiplexer.OnTouchUpdate(touchWhichWillEnd);
+            touchWhichWillEnd.phase = TouchPhase.Moved;
+            _touchDemultiplexer.OnTouchUpdate(touchWhichWillEnd);
+            touchWhichWillEnd.phase = TouchPhase.Ended;
+            _touchDemultiplexer.OnTouchUpdate(touchWhichWillEnd);
+            
+            // This call must be ignored by the touch demultiplexer, if it is not ignored, and he output action is
+            // called, then the demultiplexer does not work correctly.
+            touchWhichWillEnd.phase = TouchPhase.Began;
+            _touchDemultiplexer.OnTouchUpdate(touchWhichWillEnd);
+
+            Assert.AreEqual(expectedCallCount, callCount);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/TouchDemultiplexer.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
+namespace Balloondle.UI
+{
+    public class TouchDemultiplexer
+    {
+        private Queue<Action<Touch>> _queue;
+        private Dictionary<long, Action<Touch>> _selectedTouches;
+
+        // TODO: Add clean up method for removing touches which got invalidated without the Demultiplexer knowing.
+        
+        public TouchDemultiplexer()
+        {
+            _queue = new Queue<Action<Touch>>();
+            _selectedTouches = new Dictionary<long, Action<Touch>>();
+        }
+
+        /// <summary>
+        /// Look for reserving a touch, which will be transferred to the specified output action.
+        /// </summary>
+        /// <param name="outputAction">Action which will be called on all the touches' update calls.</param>
+        public void AddOutputToQueue(Action<Touch> outputAction)
+        {
+            _queue.Enqueue(outputAction);
+        }
+
+        /// <summary>
+        /// Called for each touch's instance situation on a single frame.
+        /// </summary>
+        /// <param name="touch"></param>
+        public void OnTouchUpdate(Touch touch)
+        {
+            if (_selectedTouches.ContainsKey(touch.touchId))
+            {
+                TransferTouchToSelectedOutput(touch);
+
+                if (!IsTouchPhaseStillAlive(touch))
+                {
+                    RemoveTouchFromSelectedTouches(touch);
+                }
+            }
+            else
+            {
+                // Ignore if touch phase does not indicate it still exists.
+                if (!IsTouchPhaseStillAlive(touch))
+                {
+                    return;
+                }
+
+                // Ignore touch since there is no one in the queue.
+                if (_queue.Count == 0)
+                {
+                    return;
+                }
+                
+                SelectOutputFromQueueForTouch(touch);
+                TransferTouchToSelectedOutput(touch);
+            }
+        }
+
+        private void TransferTouchToSelectedOutput(Touch touch)
+        {
+            if (!_selectedTouches.ContainsKey(touch.touchId))
+            {
+                throw new InvalidOperationException("Cannot transfer touch to a non-existing output.");
+            }
+
+            _selectedTouches[touch.touchId].Invoke(touch);
+        }
+
+        private static bool IsTouchPhaseStillAlive(Touch touch)
+        {
+            return touch.phase == TouchPhase.Began || 
+                   touch.phase == TouchPhase.Moved ||
+                   touch.phase == TouchPhase.Stationary;
+        }
+
+        private void SelectOutputFromQueueForTouch(Touch touch)
+        {
+            // Ignore request if empty.
+            if (_queue.Count == 0)
+            {
+                return;
+            }
+            
+            Action<Touch> output = _queue.Dequeue();
+
+            _selectedTouches.Add(touch.touchId, output);
+        }
+
+        private void RemoveTouchFromSelectedTouches(Touch touch)
+        {
+            if (!_selectedTouches.ContainsKey(touch.touchId))
+            {
+                throw new InvalidOperationException("Unable to remove an unselected touch.");
+            }
+
+            _selectedTouches.Remove(touch.touchId);
+        }
+    }
+}
