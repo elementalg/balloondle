259cd0dfaf600d5648b104269057c1250eb1ea65
Gabriel Ami <gamihalachioaie@gmail.com>
2021-08-04 15:42:10 +0200

    add: break rope when a joint breaks

---
+++ Assets/Scripts/Gameplay/RopeJointController.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+
+namespace Balloondle.Gameplay
+{
+    public class RopeJointController : MonoBehaviour
+    {
+        private HingeJoint2D _hingeJoint2D;
+        private DistanceJoint2D _distanceJoint2D;
+
+        public Rope RopeInstance { get; set; }
+        
+        private void Start()
+        {
+            if (GetComponent<HingeJoint2D>() == null)
+            {
+                throw new InvalidOperationException(
+                    "RopeJointController requires the GameObject to contain a HingeJoint2D component.");
+            }
+
+            if (GetComponent<DistanceJoint2D>() == null)
+            {
+                throw new InvalidOperationException(
+                    "RopeJointController requires the GameObject to contain a DistanceJoint2D component.");
+            }
+
+            _hingeJoint2D = GetComponent<HingeJoint2D>();
+            _distanceJoint2D = GetComponent<DistanceJoint2D>();
+        }
+
+        private void OnJointBreak2D(Joint2D brokenJoint)
+        {
+            if (brokenJoint == _hingeJoint2D)
+            {
+                _distanceJoint2D.enabled = false;
+            }
+            else
+            {
+                _hingeJoint2D.enabled = false;
+            }
+
+            if (RopeInstance != null)
+            {
+                RopeInstance.OnRopeJointBreak();
+            }
+        }
+    }
+}
