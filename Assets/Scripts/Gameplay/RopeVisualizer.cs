08ab6d65dd01604dab346ff97a815d8f465ddf3b
Gabriel Ami <gamihalachioaie@gmail.com>
2021-08-06 19:08:41 +0200

    fix: index limit error

--- Assets/Scripts/Gameplay/RopeVisualizer.cs
+++ Assets/Scripts/Gameplay/RopeVisualizer.cs
@@ -43,7 +43,7 @@ namespace Balloondle.Gameplay
             {
                 _spriteShapeController.spline.Clear();
 
-                for (int i = 0; i <= _ropePoints.Count; i++)
+                for (int i = 0; i < _ropePoints.Count; i++)
                 {
                     Transform pointTransform = _ropePoints[i];
 
