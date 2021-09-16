445d5444bf26b837d6cafa22f11dee23c7cd849c
Gabriel Ami <gamihalachioaie@gmail.com>
2021-09-16 11:36:51 +0200

    add: use WorldEntity way of spawning

--- Assets/Source/Gameplay/CharacterInitializer.cs
+++ Assets/Source/Gameplay/CharacterInitializer.cs
@@ -1,30 +1,19 @@
-using Balloondle.Gameplay.Physics2D;
 using UnityEngine;
 
 namespace Balloondle.Gameplay
 {
     public class CharacterInitializer : MonoBehaviour
     {
-        [SerializeField] private Rope2DSpawner m_Rope2DSpawner;
-
-        [SerializeField] private GameObject m_StartGameObject;
-        [SerializeField] private Rigidbody2D m_EndBody;
-        
-        [SerializeField] private float m_EndPointJointBreakForce = 10f;
-        [SerializeField] private float m_EndPointJointBreakTorque = 10f;
-        [SerializeField] private float m_RopeCellJointBreakForce = 2f;
-        [SerializeField] private float m_RopeCellJointBreakTorque = 2f;
-        [SerializeField] private float m_JointBetweenEndsBreakForce = 1000f;
-        
-        // Start is called before the first frame update
         void Start()
         {
-            Rope2DLimits ropeLimits = new Rope2DLimits(5f, m_EndPointJointBreakForce, 
-                m_EndPointJointBreakTorque, m_RopeCellJointBreakForce,
-                m_RopeCellJointBreakTorque, m_JointBetweenEndsBreakForce, 100f);
+            WorldEntitySpawner worldEntitySpawner = GetComponent<WorldEntitySpawner>();
+            WorldEntity balloon = worldEntitySpawner.Spawn("Balloon", Vector3.zero, Quaternion.identity);
+            WorldEntity weapon = worldEntitySpawner.Spawn("GreenVenom",
+                new Vector3(0, -2.95f, 0f), 
+                Quaternion.Euler(0, 0, 180f));
             
-            m_Rope2DSpawner.CreateRopeConnectingTwoRigidBodies2D(m_StartGameObject.GetComponent<Rigidbody2D>(),
-                new Vector2(0, -0.5f), m_EndBody, new Vector2(0, -0.32f), ropeLimits);
+            worldEntitySpawner.Attacher.Attach(balloon, new Vector3(0, -0.5f, 0f),
+                weapon, new Vector3(0, -0.32f, 0f));
         }
     }
 }
