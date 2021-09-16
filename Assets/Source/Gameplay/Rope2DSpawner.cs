20c6b04a6690699e80b3c86f713eeb41b237307f
Gabriel Ami <gamihalachioaie@gmail.com>
2021-09-16 11:10:44 +0200

    add: turn into a ScriptableObject and calculate maximum distance

--- Assets/Source/Gameplay/Rope2DSpawner.cs
+++ Assets/Source/Gameplay/Rope2DSpawner.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 namespace Balloondle.Gameplay
 {
-    public class Rope2DSpawner : MonoBehaviour
+    [CreateAssetMenu(fileName = "Rope2DSpawnerConfig", menuName = "Rope2D/Spawner", order = 1)]
+    public class Rope2DSpawner : ScriptableObject
     {
         private const string RopeTag = "Rope";
         private const string RopeGameObjectName = "SpawnedRope";
         
-        [SerializeField] private GameObject m_RopeCellPrefab;
-        [SerializeField] private GameObject m_RopeCellSpriteShapePrefab;
+        [SerializeField] private GameObject m_Rope2DCellPrefab;
+        [SerializeField] private GameObject m_Rope2DCellSpriteShapePrefab;
+        [SerializeField, Tooltip("Multiplier applied to the current distance in order to define the maximum distance.")]
+        private float m_Rope2DMaximumDistanceMultiplier = 1.25f;
         
         public void CreateRopeConnectingTwoRigidBodies2D(Rigidbody2D start, Vector2 startAnchor, Rigidbody2D end,
             Vector2 endAnchor, Rope2DLimits limits)
@@ -26,7 +29,10 @@ namespace Balloondle.Gameplay
                 name = RopeGameObjectName
             };
 
-            Transform ropeTransform = GetComponent<Transform>();
+            limits.MaximumDistanceBetweenBodies = Vector2
+                .Distance(start.GetPoint(startAnchor), end.GetPoint(endAnchor)) * m_Rope2DMaximumDistanceMultiplier;
+            
+            Transform ropeTransform = ropeGameObject.GetComponent<Transform>();
             ropeTransform.position = Vector3.zero;
             ropeTransform.rotation = Quaternion.identity;
 
@@ -34,11 +40,11 @@ namespace Balloondle.Gameplay
             
             Rope2D rope = ropeGameObject.AddComponent<Rope2D>();
             rope.Limits = limits;
-            rope.RopeCellPrefab = m_RopeCellPrefab;
+            rope.RopeCellPrefab = m_Rope2DCellPrefab;
             rope.AddCellsForJoiningStartToEnd(start.gameObject, startAnchor, end, endAnchor);
 
             Rope2DVisualizer rope2DVisualizer = ropeGameObject.AddComponent<Rope2DVisualizer>();
-            rope2DVisualizer.RopeSpriteShapePrefab = m_RopeCellSpriteShapePrefab;
+            rope2DVisualizer.RopeSpriteShapePrefab = m_Rope2DCellSpriteShapePrefab;
             rope2DVisualizer.VisualizeRope();
 
             ropeGameObject.AddComponent<Rope2DWorldEntityConfigurator>();
