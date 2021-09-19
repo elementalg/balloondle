c0bfc9618dc86abf87c9e0595df5584a60319774
Gabriel Ami <gamihalachioaie@gmail.com>
2021-09-19 05:48:25 +0200

    add: entries ends logic

---
+++ Assets/Source/Script/EntriesEndsHandler.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Balloondle.Script
+{
+    public abstract class EntriesEndsHandler : ScriptableObject
+    {
+        public abstract void OnEntryStart(ulong entryId);
+        public abstract void OnEntryEnd(ulong entryId);
+    }
+}
