89d7951b24a9d6568a508a125e728be2ea379376
Gabriel Ami <gamihalachioaie@gmail.com>
2021-09-19 05:47:47 +0200

    add: wait for movement on first run script

---
+++ Assets/Source/Script/Handlers/EntriesEnds/WaitForMovementEntriesEndsHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Balloondle.Gameplay;
+using UnityEngine;
+
+namespace Balloondle.Script.Handlers.EntriesEnds
+{
+    [CreateAssetMenu(fileName = "WaitForMovementEntriesEndsHandler",
+        menuName = "Script/Entries Ends Handler/Wait For Movement", order = 1)]
+    public class WaitForMovementEntriesEndsHandler : EntriesEndsHandler
+    {
+        [SerializeField, Tooltip("Prefab containing the joystick container.")]
+        private GameObject m_JoystickContainerPrefab;
+        
+        public override void OnEntryStart(ulong entryId)
+        {
+            if (entryId == 0)
+            {
+                Canvas canvas = FindObjectOfType<Canvas>();
+
+                if (canvas == null)
+                {
+                    throw new InvalidOperationException("Canvas could not be found.");
+                }
+                
+                Instantiate(m_JoystickContainerPrefab, canvas.transform);
+                
+                FindObjectOfType<MovementController>().OnCharacterMove += () =>
+                {
+                    FindObjectOfType<ScriptDirector>().TryTriggerExpireEvent("player-moved");
+                };
+            }
+        }
+
+        public override void OnEntryEnd(ulong entryId)
+        {
+            
+        }
+    }
+}
