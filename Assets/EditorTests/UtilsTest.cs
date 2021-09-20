b58bae62d5307e92e70d9e1b4b3626c2911943fe
Gabriel Ami <gamihalachioaie@gmail.com>
2021-09-20 16:07:23 +0200

    add: negative if odd

---
+++ Assets/EditorTests/UtilsTest.cs
@@ -0,0 +1,21 @@
+﻿using Balloondle;
+using NUnit.Framework;
+
+namespace EditorTests
+{
+    public class UtilsTest
+    {
+        [Test]
+        public void NegativeIfOddRunsCorrectly()
+        {
+            Assert.AreEqual(1, Utils.NegativeIfOdd(-2));
+            Assert.AreEqual(-1, Utils.NegativeIfOdd(-1));
+            Assert.AreEqual(1, Utils.NegativeIfOdd(0));
+            Assert.AreEqual(-1, Utils.NegativeIfOdd(1));
+            Assert.AreEqual(1, Utils.NegativeIfOdd(2));
+            Assert.AreEqual(-1, Utils.NegativeIfOdd(3));
+            Assert.AreEqual(1, Utils.NegativeIfOdd(4));
+            Assert.AreEqual(-1, Utils.NegativeIfOdd(5));
+        }
+    }
+}
---
+++ Assets/Source/Utils.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Balloondle
+{
+    public static class Utils
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="i">Number to be checked.</param>
+        /// <returns><b>-1</b> if <i>i</i> is odd, <b>1</b> otherwise.</returns>
+        public static int NegativeIfOdd(int i)
+        {
+            return (1 + 2 * (Mathf.Abs(i) % 2 * -1));
+        }
+    }
+}
