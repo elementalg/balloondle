def79e1888ad4b2fef4c2f2678a7cdc54f2f5230
Gabriel Ami <gamihalachioaie@gmail.com>
2021-09-20 21:25:48 +0200

    refactor: use int for entries' ids

--- Assets/Source/Script/Core/CharacterEntry.cs
+++ Assets/Source/Script/Core/CharacterEntry.cs
@@ -5,13 +5,13 @@
         public string Text { get; }
         public Character CharacterData { get; }
         
-        public CharacterEntry(ulong id, float duration, string text, Character character) : base(id, duration)
+        public CharacterEntry(int id, float duration, string text, Character character) : base(id, duration)
         {
             Text = text;
             CharacterData = character;
         }
 
-        public CharacterEntry(ulong id, float duration, ExpireEvent expireEvent, string text, Character character) : 
+        public CharacterEntry(int id, float duration, ExpireEvent expireEvent, string text, Character character) : 
             base(id, duration, expireEvent)
         {
             Text = text;
