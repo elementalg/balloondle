e8eddef32ce8ac8513a38b35f000c25ac19bedd7
Gabriel Ami <gamihalachioaie@gmail.com>
2021-09-21 22:59:06 +0200

    add: basic structure for effects

---
+++ Assets/Source/Effect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Balloondle
+{
+    [Serializable]
+    public struct Effect
+    {
+        public string Name;
+        public bool IsOnlySound;
+        public GameObject Prefab;
+        public AudioClip Sound;
+    }
+}
---
+++ Assets/Source/EffectsDefinition.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Balloondle
+{
+    [CreateAssetMenu(fileName = "EffectsDefinition", menuName = "Effects/Effects Definition", order = 0)]
+    public class EffectsDefinition : ScriptableObject
+    {
+        public List<Effect> m_Effects;
+    }
+}
