30fe312cd7419fc5267b600c5d70c6ad94266d30
Gabriel Ami <gamihalachioaie@gmail.com>
2021-09-24 13:28:09 +0200

    add: handle out of bounds

---
+++ Assets/Source/Gameplay/OutOfBoundsHandler.cs
@@ -0,0 +1,32 @@
+﻿using Balloondle.Gameplay.World;
+using UnityEngine;
+
+namespace Balloondle.Gameplay
+{
+    public class OutOfBoundsHandler : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            // Move player to spawn.
+            if (other.gameObject.GetComponent<Player>() != null)
+            {
+                other.gameObject.transform.SetPositionAndRotation(new Vector3(8.47f, 1.79f, 0f), Quaternion.identity);
+                return;
+            }
+
+            // Move weapon to spawn.
+            if (other.gameObject.GetComponent<Weapon>() != null)
+            {
+                other.gameObject.transform.SetPositionAndRotation(new Vector3(10.47f, 1.79f, 0f), Quaternion.identity);
+                return;
+            }
+
+            if (other.gameObject.GetComponent<WorldEntity>() != null)
+            {
+                WorldEntity entity = other.gameObject.GetComponent<WorldEntity>();
+                entity.Damage(entity.Health);
+                return;
+            }
+        }
+    }
+}
